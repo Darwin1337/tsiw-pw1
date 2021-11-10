c98a483bf52e7d86cf47c50778e944aa6f7a71c4
Diogo Borges <40200353@esmad.ipp.pt>
2021-11-10 22:09:21 +0000

    added ex3

---
+++ ex3-vuepropcal/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Person />
+  </div>
+</template>
+
+<script>
+import Person from './components/Person.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Person
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ ex3-vuepropcal/src/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p v-for="(detail, prop) of person" :key="prop">{{ prop + ": " + detail}}</p>
+    {{ printDataPerson() }}
+    {{ printDataPerson() }}
+    {{ printDataPersonComputed }}
+    {{ printDataPersonComputed }}
+    <p>fullName: {{ getFullNameComputed }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      person: { firstName: "Rui", lastName: "Silva", age: 23 }
+    }
+  }, created () {
+    console.log("Evento: Criação");
+  }, mounted () {
+    console.log("Evento: Montagem");
+  }, updated () {
+    console.log("Evento: Atualização");
+  }, methods: {
+    printDataPerson() {
+      console.log("METHOD--> NOME: " + this.person.firstName + " e IDADE: " + this.person.age);  
+    }
+  }, computed: {
+    printDataPersonComputed() {
+      console.log("COMPUTED--> NOME: " + this.person.firstName + " e IDADE: " + this.person.age);
+      return null;
+    }, getFullNameComputed() {
+      return this.person.firstName + " " + this.person.lastName;
+    }
+  }, watch: {
+    'person.age'(newValue, oldValue) {
+      console.log("Idade alterada de " + oldValue + " para " + newValue);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
