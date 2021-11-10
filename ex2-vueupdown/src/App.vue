ff5de4950b02e32e10dc35e47dc1f72a477c6f6a
Diogo Borges <40200353@esmad.ipp.pt>
2021-11-10 20:20:55 +0000

    added ex2

---
+++ ex2-vueupdown/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Ex1 />
+  </div>
+</template>
+
+<script>
+import Ex1 from './components/Ex1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ex1
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ ex2-vueupdown/src/components/Ex1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      <span>{{ num }}</span>
+    </div>
+    <div>
+      <button @click="num++">+</button>
+      <button @click="num--" :disabled="num == 0">-</button>
+    </div>
+    <h2 v-if="num < 0">Número inválido</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: localStorage.num ? JSON.parse(localStorage.num) : 0
+    }
+  }, watch: {
+    num() {
+      // if (this.num < 0) { this.num = 0; }
+      localStorage.setItem("num", JSON.stringify(this.num));
+    }
+  }, created () {
+    console.log("A instância Vue foi criada");
+  }, mounted () {
+    console.log("A instância Vue foi montada");
+  }, updated () {
+    console.log("O número foi alterado");
+  }
+}
+</script>
+
+<style>
+
+</style>
