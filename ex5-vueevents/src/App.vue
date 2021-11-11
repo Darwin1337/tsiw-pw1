0ceb05d82fb8b777d9a6ba850aaad5bc194f8e0d
Diogo Borges <40200353@esmad.ipp.pt>
2021-11-11 19:36:33 +0000

    added ex5

---
+++ ex5-vueevents/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ ex5-vueevents/src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <div>
+        <button @click="showMsg()">um</button>
+        <button @click="showCustomMsg(msg, $event)">dois</button>
+      </div>
+      <div v-on:click="showMessage">
+          <!-- <button v-on:click="showMessage">CLICA-ME</button>
+          <button v-on:click.stop="showMessage">CLICA-ME</button>
+          <button v-on:click.once="showMessage">CLICA-ME</button> -->
+          <button @keyup.tab="showMessage">CLICA-ME</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Programação Web I"
+        }
+    }, methods: {
+        showMsg() {
+            alert(this.msg);
+        },
+        showCustomMsg(msg, event) {
+            alert(msg);
+            console.log(event.target);
+            console.log(event.type);
+        }
+    }, computed: {
+        showMessage() {
+            console.log("hey");
+            return "";
+        }
+    }
+}
+</script>
+
+<style>
+</style>
